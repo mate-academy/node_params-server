862c10db7cd405909a556761df2f466b99e4144b
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2022-01-26 21:35:03 +0100

    Solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,50 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const getDefaultResponse = () => {
+  return '<h1>Welcome!</h1>'
+    + '<p>This server application creates  and returns JSON from URL</p>'
+    + '<p>Write your URL in search bar and get your JSON!</p>';
+};
+
+const getUrlResponse = (normilizedPath, normilizedURL) => {
+  const parts = normilizedPath.split('/');
+  const query = {};
+
+  for (const key of normilizedURL.searchParams.keys()) {
+    if (!query[key]) {
+      query[key] = normilizedURL.searchParams.getAll(key);
+    }
+  }
+
+  return JSON.stringify({
+    parts,
+    query,
+  });
+};
+
+const server = http.createServer((req, res) => {
+  const normilizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const normilizedPath = normilizedURL.pathname.slice(1);
+
+  if (!normilizedPath) {
+    const response = getDefaultResponse();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end(response);
+  } else if (normilizedPath !== 'favicon.ico') {
+    const response = getUrlResponse(normilizedPath, normilizedURL);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(response);
+  } else {
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
