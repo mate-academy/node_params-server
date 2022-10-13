3e6cea5faa973b2caacddd16bd24f0513c3d8c87
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-10-13 14:58:09 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 8080;
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
+  if (normalizedURL.pathname !== '/favicon.ico') {
+    const response = {
+      paths: normalizedURL.pathname.split('/').slice(1),
+      query: Object.fromEntries(normalizedURL.searchParams.entries()),
+    };
+
+    console.log(response);
+    res.write(`\n${JSON.stringify(response)}`);
+
+    fs.writeFile(`./src/response${new Date()
+      .toLocaleTimeString().split(':').join('')}.json`,
+    JSON.stringify(response), (er) => {
+      if (er) {
+        console.log('Error occured');
+      }
+    });
+  }
+
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
