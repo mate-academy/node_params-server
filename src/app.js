cc7dff7687ae8cb27b89a7adaac285894206bf96
Roma Artiukh <romanskarb@ukr.net>
2022-10-18 09:03:20 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint-disable no-console */
 
 /**
  * Implement sum function:
@@ -13,9 +14,28 @@
  *
  * @return {number}
  */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+// function sum(a, b) {
+//   // write code here
+//   return a + b;
+// }
 
-module.exports = sum;
+// module.exports = sum;
+
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const obj = {};
+
+const server = http.createServer((req, res) => {
+  const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
+
+  const params = Object.fromEntries(normalizeURL.searchParams.entries());
+
+  obj.parts = normalizeURL.pathname.split('/');
+  obj.query = params;
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
