819667f038bb5a26e126328fac43d29b360601e3
Svyatoslav <svyatos1337@gmail.com>
2022-10-18 23:06:32 +0300

    fixed toggle parts value

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
 
-  const parts = req.url.slice(1).split('?')[0].split('/');
+  const parts = normalizedURL.pathname.slice(1).split('/');
   const query = Object.fromEntries(
     normalizedURL.searchParams.entries());
 
