acfc837a5beb4904e62637104ec5b984d309b721
lizapolokhina <lizapolohina@gmail.com>
2022-10-25 13:30:34 +0200

    changed

--- src/app.js
+++ src/app.js
@@ -8,15 +8,13 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
 
-  const parts = req.url.slice(1).split('?')[0].split('/');
+  const parts = normalizedURL.pathname.slice(1).split('/');
   const query = Object.fromEntries(
     normalizedURL.searchParams.entries());
 
   const result = JSON.stringify({
     parts, query,
   });
-
-  console.log(result);
 
   res.end();
 
