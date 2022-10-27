bf86b2b64356879769925ca95ad19709e4f9c0e3
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-10-27 15:42:10 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -5,19 +5,16 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
 
-  console.log(normalizedURL); // eslint-disable-line no-console
-
   const data = {
-    parts: normalizedURL.pathname
-      .slice(1)
-      .split('/'),
+    parts: normalizedURL.pathname.slice(1).split('/'),
+    params: Object.fromEntries(normalizedURL.searchParams.entries()),
   };
 
   const params = Object.fromEntries(normalizedURL.searchParams.entries());
 
   data.query = params;
 
-  res.end();
+  res.end(JSON.stringify(data));
 });
 
 server.listen(PORT, () => {
