48d36cf8c5f89f3c5bd0c58d72c8e56aa51dc86d
Max Bovtun <maxbovtun@gmail.com>
2022-10-29 17:32:18 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+// Get port from env or use default:
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer((request, response) => {
+  // We'll answer only on requests with a 'text/html' accept header:
+  if (request.headers.accept.includes('text/html')) {
+    // Reconstruct URL to avoid using url module...well...
+    const url = new URL(
+      request.url, request.protocol + '://' + request.headers.host + '/'
+    );
+    const urlQuery = new URLSearchParams(url.search);
+    const query = {};
+
+    // Get search params from URL and convert it to object:
+    for (const [key, value] of urlQuery.entries()) {
+      query[key] = value;
+    }
+
+    // Return JSON with all required data:
+    response.end(JSON.stringify({
+      'parts': url.pathname.split('/').slice(1),
+      query,
+    }));
+  }
+});
+
+// Server listener and message with info:
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running on: http://localhost:' + PORT);
+});
