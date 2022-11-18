2b62a244bc0bb82a7c5a7563c49a67a77da7dac4
Artem Musii <musiy.artem@gmail.com>
2022-11-19 01:00:50 +0200

    params server implemented

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const url = require('url');
 
-module.exports = sum;
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+
+  let parts = [];
+
+  if (normalizedUrl.pathname) {
+    parts = normalizedUrl.pathname.split('/').slice(1);
+  }
+
+  let query = '';
+
+  if (normalizedUrl.searchParams) {
+    query = Object.fromEntries(normalizedUrl.searchParams.entries());
+  }
+
+  const data = {
+    parts,
+    query,
+  };
+
+  res.end(JSON.stringify(data));
+});
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+
+module.exports = {
+  server,
+};
