4a7536bf39b1e953d61899d9b5c6d71db5e23abf
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-11-18 16:38:01 +0200

    Implemented the server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const { getUrlComponents } = require('./getUrlComponents');
 
-module.exports = sum;
+const PORT = process.env.PORT || '8080';
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const { parts, query } = getUrlComponents(req);
+  const response = {};
+
+  if ((parts[0]) !== '') {
+    response.parts = parts;
+  }
+
+  if (Object.keys(query).length) {
+    response.query = query;
+  }
+
+  res.end(JSON.stringify(response));
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server is running on: http://localhost:${PORT}`);
+});
