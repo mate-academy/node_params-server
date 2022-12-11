dcc68b0d1acd6d9becd3653f615cc2dbb784cb64
OleksandrHrachov <ol.grachov@gmail.com>
2022-12-11 13:32:13 +0200

    solution

--- src/paramsServer.js
+++ src/paramsServer.js
@@ -21,5 +21,6 @@ const server = http.createServer((req, resp) => {
 });
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${PORT}`);
 });
