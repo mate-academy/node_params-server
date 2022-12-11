69b086baca69196649c8390f401ceaa4356784eb
OleksandrHrachov <ol.grachov@gmail.com>
2022-12-11 13:31:40 +0200

    solution

---
+++ src/paramsServer.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, resp) => {
+  const normalizeUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  const respObj = {};
+
+  const parts = normalizeUrl.pathname.slice(1).split('/');
+
+  respObj.parts = parts;
+
+  const queryObj = Object.fromEntries(normalizeUrl.searchParams.entries());
+
+  respObj.query = queryObj;
+
+  resp.end(JSON.stringify(respObj));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
