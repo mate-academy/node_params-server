7a988b62ef93188d8b29e13c6f286a122495894e
Vladyslav <vladyslav3177@gmail.com>
2023-01-16 00:32:38 +0200

    feat(app): added the ability to get params server

--- src/app.js
+++ src/app.js
@@ -13,9 +13,25 @@
  *
  * @return {number}
  */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+  // console.log('normalizedUrl => ', normalizedUrl);
+
+  const partsArray = normalizedUrl.pathname.split('/').slice(1);
+  const params = Object.fromEntries(normalizedUrl.searchParams);
+
+  res.end(JSON.stringify({
+    parts: partsArray,
+    query: params,
+  }));
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`The server is listening on port ${PORT}`);
+});
