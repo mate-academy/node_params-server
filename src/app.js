0e2b11a897befab0b6f12f0cc42f74ceaa3232f7
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-01 21:38:38 +0200

    made variables

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
 
+  const parts = normalizedURL.pathname.slice(1).split('/');
+  const query = Object.fromEntries(normalizedURL.searchParams.entries());
+
   res.end(JSON.stringify({
-    parts: normalizedURL.pathname.slice(1).split('/'),
-    query: Object.fromEntries(normalizedURL.searchParams.entries()),
+    parts,
+    query,
   }));
 });
 
