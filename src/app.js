670e3fa1ee4f6ed077b3964a70ee2eda8c3c98f4
Vasya Rymarenko <vrima60@gmail.com>
2023-02-16 23:06:38 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -13,9 +13,19 @@
  *
  * @return {number}
  */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+  const parts = normalizedURL.pathname.slice(1).split('/');
+  const query = Object.fromEntries(normalizedURL.searchParams.entries());
+
+  res.end(JSON.stringify({
+    parts,
+    query,
+  }));
+});
+
+server.listen(PORT);
