f0bb0944cbe3e52ac16623de32cdf0557c5a49be
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-17 13:51:04 +0200

    destructure normalizedUrl

--- src/app.js
+++ src/app.js
@@ -4,25 +4,29 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  const normalizedUrl = new URL(req.url, `https://${req.headers.host}`);
+  const {
+    search,
+    searchParams: { get, getAll },
+    pathname,
+  } = new URL(req.url, `https://${req.headers.host}`);
 
   const getParam = param => ({
-    [param]: (normalizedUrl.search.match(RegExp(param, 'g')) || []).length > 1
-      ? normalizedUrl.searchParams.getAll(param)
-      : normalizedUrl.searchParams.get(param),
+    [param]: (search.match(RegExp(param, 'g')) || []).length > 1
+      ? getAll(param)
+      : get(param),
   });
 
-  if (!normalizedUrl.search) {
+  if (!search) {
     res.send('No params');
     next();
   }
 
-  res.json(normalizedUrl.search.slice(1)
+  res.json(search.slice(1)
     .split('&')
     .map(entry => entry.split('=')[0])
     .reduce((acc, curr) => ({
       ...acc,
-      parts: normalizedUrl.pathname.slice(1).split('/'),
+      parts: pathname.slice(1).split('/'),
       query: {
         ...acc.query,
         ...getParam(curr),
