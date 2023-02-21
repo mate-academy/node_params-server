0a381524ddb3e29ae5316a55f9826d88034b4944
Demut <uddarendeli1@gmail.com>
2023-02-21 20:35:14 +0300

    switched to commonjs

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'application/json');
+
+  const qIndex = req.url.indexOf('?');
+  const parts = req.url.slice(1, qIndex).split('/');
+  const query = {};
+
+  for (const part of req.url.slice(qIndex + 1).split('&')) {
+    const split = part.split('=');
+
+    if (split[0] in query) {
+      query[split[0]] = [...query[split[0]], split[1]];
+    } else {
+      query[split[0]] = [split[1]];
+    }
+  }
+
+  // finishes the response
+  res.end(JSON.stringify({
+    parts,
+    query,
+  }));
+});
+
+// Enables the server
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
