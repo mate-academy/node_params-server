2da623e5f4c4d634b70bd62d0f590aaf21a3ff7d
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-23 13:02:33 +0100

    solution

---
+++ src/request.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+// this file is created to check whether app works
+'use strict';
+
+const axios = require('axios');
+
+const BASE = 'http://localhost:3000';
+const pathname = '/hello/world/123';
+const search = '?x=1&search=some&x=4';
+
+axios.get(BASE + pathname + search)
+  .catch(() => console.log('Error'));
