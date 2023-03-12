3337aa8b2d59a97efdfb73b1f33d0e4bbc811535
Ivan Stadnytskyy <vanst1@icloud.com>
2023-03-13 00:04:39 +0200

    Solution FIX

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const http = require('http');
+const getQuery = require('./getQuery');
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   const normalizedURL = new URL(req.url, `http//${req.headers.host}`);
   const parts = normalizedURL.pathname.slice(1).split('/');
-  const query = Object.fromEntries(normalizedURL.searchParams.entries());
+  const searchParams = normalizedURL.searchParams;
+  const query = getQuery(searchParams);
 
   res.end(JSON.stringify({
     parts,
