b3fc442421f8dce4af6e60d545d9d1839cb3a90e
Deniskakaka <krollik1000000@gmail.com>
2023-04-29 20:06:24 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normilezedURL = new URL(req.url, `http://${req.headers.host}`);
+  const pathname = normilezedURL.pathname.split('/').filter(el => el !== '');
+  const params = Object.fromEntries(normilezedURL.searchParams.entries());
+
+  res.end(JSON.stringify({
+    parts: pathname,
+    query: params,
+  }));
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server Start');
+});
