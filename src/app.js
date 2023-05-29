05a3c836b3c0aefc8ad2abc312aee3ebc1b24d06
Daria <hamdsad8@gmail.com>
2023-05-29 14:14:33 +0300

    created params server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const url = require('url');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+
+  const result = {
+    'parts': [],
+    'query': {
+    },
+  };
+
+  const pathname = normalizedUrl.pathname;
+  const parts = pathname.split('/').filter((el) => el !== '');
+
+  result.parts.push(...parts);
+
+  if (normalizedUrl.searchParams) {
+    for (const [key, value] of normalizedUrl.searchParams.entries()) {
+      if (result.query.hasOwnProperty(key)) {
+        if (Array.isArray(result.query[key])) {
+          result.query[key].push(value);
+        } else {
+          result.query[key] = [result.query[key], value];
+        }
+      } else {
+        result.query[key] = value;
+      }
+    }
+  }
+
+  res.end();
+
+  return JSON.stringify(result);
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
