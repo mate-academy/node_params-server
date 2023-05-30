f4dd1c4736c6cee9e324946d593f0ba2c8e5403a
Olga Bordiug <obordiug@gmail.com>
2023-05-30 17:56:22 +0300

    solution after comments

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  const normolizeURL = new URL(req.url, `http://${req.headers.host}`);
-  const parts = normolizeURL.pathname.split('/');
-  const query = Object.fromEntries(normolizeURL.searchParams.entries());
+  const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
+  const parts = normalizeURL.pathname.split('/');
+  const query = Object.fromEntries(normalizeURL.searchParams.entries());
 
   res.setHeader('Content-Type', 'application/json');
 
@@ -16,8 +16,8 @@ const server = http.createServer((req, res) => {
     parts,
     query,
   }));
+});
 
-  server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
