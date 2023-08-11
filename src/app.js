38e671c6812b7d263f25765b620ade6fba2bf8f2
Anastasiia <svintsova.nastya@gmail.com>
2023-08-11 15:51:43 +0300

    implement params server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const url = require('url');
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const parsedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+  const parts = parsedUrl.pathname
+    .split('/')
+    .filter(part => part !== '');
+
+  const query = Object.fromEntries(parsedUrl.searchParams.entries());
+
+  const responseJson = {
+    parts,
+    query,
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(responseJson));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
