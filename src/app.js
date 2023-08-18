b294e6468f4d892c83b9c9e27d06e33b6449056a
artkas03 <zhonayer2017@gmail.com>
2023-08-18 13:02:51 +0300

    refactored code

--- src/app.js
+++ src/app.js
@@ -4,31 +4,24 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
-function createQuery(string) {
-  const paramsEntries = string
-    .split('&')
-    .map(param => param.split('='))
-    .filter(param => param[0]);
-
-  return Object.fromEntries(paramsEntries);
-}
-
 const server = http.createServer((req, res) => {
   try {
-    req.statusCode = 200;
-
     const [pathname, params] = req.url.split('?');
+
+    const urlParams = new URLSearchParams(params);
 
     const requestObject = {
       parts: pathname.split('/').filter(pathpart => pathpart),
-      query: createQuery(params),
+      query: Object.fromEntries([...urlParams.entries()]),
     };
+
+    res.statusCode = 200;
 
     res.end(JSON.stringify(requestObject));
   } catch (err) {
-    req.statusCode = 400;
+    res.statusCode = 400;
 
-    req.end(err.message);
+    res.end(err.message);
   }
 });
 
