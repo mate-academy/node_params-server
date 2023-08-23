ef1f766f65bdfae7d2441d8f60c3b00c42f2b53f
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-08-23 20:11:25 +0300

    The server has been made

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
+const { URL: NodeURL } = require('url');
 
-module.exports = sum;
+const getSearchParams = (searchParams) => {
+  const searchParamsObj = {};
+
+  searchParams.forEach((value, key) => {
+    searchParamsObj[key] = value;
+  });
+
+  return searchParamsObj;
+};
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'application/json');
+
+  const newUrl = new NodeURL(req.url, `http://${req.headers.host}`);
+  const searchParams = newUrl.searchParams;
+  const parts = newUrl.pathname.slice(1).split('/');
+
+  const jsonResponse = {
+    'parts': parts[0].length !== 0 ? parts : 'Please provide pathname',
+    'query': newUrl.search.length !== 0
+      ? getSearchParams(searchParams)
+      : 'Please provide search params',
+  };
+
+  res.end(JSON.stringify(jsonResponse));
+});
+
+server.listen('3000', () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started');
+});
