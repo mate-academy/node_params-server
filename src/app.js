9aed9ebabdb7627fe5d993e7e8981791e77c61eb
AndruhaMan <andreysut12@gmail.com>
2023-08-25 22:43:48 +0300

    The task is completed

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+  const pathnameArray = normalizedUrl.pathname.split('/').slice(1);
+  const queryParams = Object.fromEntries(normalizedUrl.searchParams.entries());
+
+  const resJSON = JSON.stringify({
+    pathname: pathnameArray,
+    query: queryParams,
+  });
+
+  res.end(resJSON);
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
