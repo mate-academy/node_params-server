ae52eeead3627adafc72699385012a5c14d52ec0
diamat1997 <diamat1997@gmail.com>
2023-11-14 15:23:50 +0200

    add task solution

---
+++ src/request.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+'use strict';
+
+const axios = require('axios');
+
+axios.get('http://localhost:3000/hello/world/123?x=1&search=some')
+  .then(res => console.log(res.data, 'res'))
+  .catch(err => console.log(err.statusCode));
+  
