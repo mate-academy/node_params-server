79c97fa24e5ee379c274da42f517fe65741cfc6c
Mariia Bila <mariiabila@MacBook-Pro-Mariia.local>
2023-11-26 21:12:58 +0200

    add server creation

--- src/app.js
+++ src/app.js
@@ -1,21 +1,35 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  const normalizedURL = new URL(req.url, `http//${req.headers.host}`);
+  const path = new URL(req.url, `http//${req.headers.host}`).slice(1);
+  const parts = path.split('/');
+  const query = normalizedURL.search;
+  const queryArray = query.split('=');
+  const queryObject = {};
+
+  for (let i = 0; i < queryArray.length; i + 2) {
+    const key = queryArray[i];
+    const value = queryArray[i + 1];
+    const existingValue = queryObject[key];
+
+    if (existingValue) {
+      queryObject[key] = typeof existingValue === 'object'
+        ? [...existingValue, value]
+        : [existingValue, value];
+    } else {
+      queryObject[key] = value;
+    }
+  }
+
+  res.end(JSON.stringify({
+    parts,
+    queryObject,
+  }));
+});
+
+module.exports = server;
