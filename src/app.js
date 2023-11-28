5bc4f0fe3c516004d7104e19258faab1dab49439
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-11-28 20:42:24 +0100

    params server

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
+/* eslint-disable no-console */
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3003;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') {
+    res.writeHead(204, { 'Content-Type': 'image/x-icon' });
+    res.end();
+
+    return;
+  }
+
+  const [rawParts, rawQueries] = req.url.split('?');
+
+  const parts = rawParts.slice(1) ? rawParts.split('/').slice(1) : [];
+  const query = rawQueries
+    ? (rawQueries.split('&').reduce((queries, pair) => {
+      const [key, value] = pair.split('=');
+
+      queries[key] = value;
+
+      return queries;
+    }, {}))
+    : [];
+
+  const response = {
+    parts,
+    query,
+  };
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(response, null, 2));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
