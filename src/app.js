7a58340f120fe1b45538e7dc4a2b9ebd49a775d8
Oksana Drozdyk <oksanadrozdik66@gmail.com>
2023-11-29 18:49:45 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'application/json');
+
+  const { url } = req;
+  const [partsString, paramsQuery] = url.split('?');
+  const parts = partsString
+    ? partsString.slice(1).split('/')
+    : [];
+  const params = paramsQuery
+    ? paramsQuery.split('&')
+    : [];
+  const query = {};
+
+  for (const param of params) {
+    const [key, value] = param.split('=');
+
+    query[key] = value;
+  }
+
+  const response = {
+    parts,
+    query,
+  };
+
+  res.end(JSON.stringify(response));
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is running');
+});
