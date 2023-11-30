8c0948640d241f832a806bb6162528406c073782
danulyk05 <ivan.danulyk2016@gmail.com>
2023-11-30 18:44:37 +0100

    last update

--- src/app.js
+++ src/app.js
@@ -1,31 +1,19 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
-
 const http = require('http');
-const url = require('url');
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.URL(req.url, true);
+  const parsedUrl = new URL(`http://localhost:${PORT}${req.url}`);
 
   const parts = parsedUrl.pathname.split('/').filter(part => part !== '');
-  const query = parsedUrl.query;
+  const params = parsedUrl.searchParams;
+
+  const query = {};
+
+  for (const [key, value] of params.entries()) {
+    query[key] = value;
+  };
+
   const jsonResponse = {
     parts,
     query,
@@ -41,5 +29,3 @@ server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-module.exports = sum;
