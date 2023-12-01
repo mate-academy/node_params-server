aa63d8837b16c7b059ae5ed2abab63ac06dd1078
Oleh Kuper <ok1804@ukr.net>
2023-12-01 09:09:43 +0200

    add solution params server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const http = require('http');
+const url = require('url');
+
 /**
  * Implement sum function:
  *
@@ -15,7 +18,35 @@
  */
 function sum(a, b) {
   // write code here
+  const PORT = 3000;
+  const data = {
+    parts: null,
+    query: {},
+  };
+  const server = http.createServer((req, res) => {
+    const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+    const path = normalizedUrl.pathname.slice(1).split('/');
+    const queries = normalizedUrl.search.slice(1).split('&');
+
+    queries.forEach(item => {
+      const [key, value] = item.split('=');
+
+      data.query[key] = value;
+    });
+    data.parts = path;
+    res.setHeader('Content-type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(data, null, 2));
+  });
+
+  server.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is runnig on port: http://localhost:${PORT}`);
+  });
+
   return a + b;
 }
+
+sum(1, 2);
 
 module.exports = sum;
