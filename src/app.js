c030058837f5e3e42afc26ea62c15a342cc69ea7
Anna Grechana <grechanaanna@gmial.com>
2023-12-07 19:39:01 +0200

    2nd version

--- src/app.js
+++ src/app.js
@@ -4,14 +4,23 @@
 
 const http = require('http');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
   const parts = normalizedURL.pathname.split('/').slice(1);
-  const params = Object.fromEntries(normalizedURL.searchParams.entries());
+
+  const params = {};
+
+  normalizedURL.searchParams.forEach((value, key) => {
+    if (params[key]) {
+      params[key].push(value);
+    } else {
+      params[key] = [value];
+    }
+  });
 
   res.end(
     JSON.stringify({
