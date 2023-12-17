dcfde3685459169bda7fc7e7a45eae0fb149c5a9
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-12-17 13:24:06 +0200

    implement solution

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
 'use strict';
 
-/**
- * Implement sum function:
- *
- * Function takes 2 numbers and returns their sum
- *
- * sum(1, 2) === 3
- * sum(1, 11) === 12
- *
- * @param {number} a
- * @param {number} b
- *
- * @return {number}
- */
-function sum(a, b) {
-  // write code here
-  return a + b;
-}
+const http = require('http');
 
-module.exports = sum;
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const normalizedURL = new URL(req.url, `http://localhost:${PORT}`);
+  const parts = normalizedURL.pathname.length > 1
+    ? normalizedURL.pathname.slice(1).split('/')
+    : null;
+  const query = Object.fromEntries(normalizedURL.searchParams.entries());
+  const resData = {};
+
+  if (parts) {
+    resData.parts = parts;
+  }
+
+  if (Object.keys(query).length) {
+    resData.query = query;
+  }
+
+  if (Object.keys(resData).length) {
+    res.statusCode = 200;
+    res.statusText = 'OK';
+    res.end(JSON.stringify(resData));
+  } else {
+    res.statusCode = 400;
+    res.statusText = 'Bag request';
+    res.end(JSON.stringify('Invalid request URL'));
+  }
+});
+
+server.listen(PORT, () => {
+// eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
