8a34344c8f8989ca180a1c886780503ccd541616
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-01-05 22:50:04 +0200

    Revert "params server + axios"
    This reverts commit cada72dbbf440792f927448cc5c55aa72d2cd0fe.

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
-/* eslint-disable no-console */
 'use strict';
 
-const http = require('http');
+/**
+ * Implement sum function:
+ *
+ * Function takes 2 numbers and returns their sum
+ *
+ * sum(1, 2) === 3
+ * sum(1, 11) === 12
+ *
+ * @param {number} a
+ * @param {number} b
+ *
+ * @return {number}
+ */
+function sum(a, b) {
+  // write code here
+  return a + b;
+}
 
-const PORT = process.env.PORT || 8080;
-
-const server = http.createServer((req, res) => {
-  const normilizedURL = new URL(req.url, `http://${req.headers.host}`);
-  const parts = normilizedURL.pathname.slice(1).split('/');
-  const query = Object.fromEntries(normilizedURL.searchParams.entries());
-
-  res.setHeader('Content-Type', 'application/json');
-
-  res.end(JSON.stringify({
-    parts,
-    query,
-  }));
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = sum;
