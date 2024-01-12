f6164dfacace7ad53730476237e7a73a6c87fe4f
Wita-Shchurko <vitaliya.mememe@gmail.com>
2024-01-12 17:40:15 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'application/json');
+
+  const normalizeURL = new URL(req.url, `http://localhost:${PORT}`);
+  const parts = normalizeURL.pathname.slice(1).split('/');
+
+  if (parts.includes('favicon.ico')) {
+    res.end();
+  } else {
+    const query = Object.fromEntries(
+      normalizeURL.searchParams.entries()
+    );
+
+    res.statusCode = 200;
+
+    res.write(JSON.stringify({
+      parts,
+      query,
+    }));
+    res.end();
+  }
+});
+
+server.listen(PORT);
