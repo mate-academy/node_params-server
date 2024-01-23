68866d5743f772d1b3a5b34cadf8840ee3c0de7f
Adrian <adrian.krause.ak@gmail.com>
2024-01-23 16:41:48 +0100

    Created params server.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-console */
 'use strict';
+
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+
+  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+  const parts = normalizedUrl.pathname.slice(1).split('/');
+  const searchParams = normalizedUrl.searchParams;
+  const query = {};
+
+  for (const [key, value] of searchParams.entries()) {
+    query[key] = value;
+  }
+
+  res.end(JSON.stringify({
+    parts, query,
+  }));
+});
+
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
