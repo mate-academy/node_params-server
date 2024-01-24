7a2bc5570cfa62a8686944d4c17a6a47fa9a0ec6
Marcin Ligmann <marcinligmann2@gmail.com>
2024-01-24 19:31:56 +0100

    initial commit

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    // Parsowanie request URL
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const parts = url.pathname.split('/').filter(part => part !== '');
+
+    // Parsowanie zapytania
+    const query = {};
+
+    url.searchParams.forEach((value, key) => {
+      query[key] = value;
+    });
+
+    // Tworzenie JSON response
+    const response = {
+      parts,
+      query,
+    };
+
+    console.log(response);
+
+    // Ustawienie nagłówków i wysłanie response JSON
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+  });
 }
 
 module.exports = {
