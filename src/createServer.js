43779ad9f8342295837f6ce4ccf134bc435bbf4f
Alina <kozlovska.a.mail@gmail.com>
2024-02-06 18:44:32 +0100

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,26 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const relativeUrl = req.url.startsWith('//') ? req.url.slice(1) : req.url;
+    const normalizedUrl = new URL(relativeUrl, `http://${req.headers.host}`);
+    const parts = normalizedUrl.pathname.slice(1).split('/');
+    const query = Object.fromEntries(normalizedUrl.searchParams.entries());
+
+    const data = {
+      parts,
+      query,
+    };
+
+    res.end(JSON.stringify(data));
+  });
+
+  return server;
 }
 
 module.exports = {
