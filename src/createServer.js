9dd2f23a3a25c0e22a164c941982ee42b2af0e4b
avakiel <79792776+avakiel@users.noreply.github.com>
2024-03-09 16:05:16 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,40 @@
-/* eslint-disable no-console */
+
 'use strict';
 
-function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
-}
+const http = require('http');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const path = new URL(req.url, 'http://localhost/');
+    const parts
+      = path.pathname === '/'
+        ? [path.hostname]
+        : path.pathname.split('/').filter((e) => e.length > 0);
+
+    const query = {};
+
+    path.searchParams.forEach((value, key) => {
+      if (query[key] === undefined) {
+        query[key] = value;
+      } else if (Array.isArray(query[key])) {
+        query[key].push(value);
+      } else {
+        query[key] = [query[key], value];
+      }
+    });
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        parts,
+        query,
+      })
+    );
+  });
+
+  return server;
+};
 
 module.exports = {
   createServer,
