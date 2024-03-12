25736c2194aff687b8cf708e5aac757c3bf04553
Nataliia Henda <nataliehenda1296@gmail.com>
2024-03-12 10:07:39 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,24 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const [pathName, searchQuery] = req.url.split('?');
+    const parts = pathName
+      .split('/')
+      .filter(part => !!part);
+    const params = new URLSearchParams(searchQuery);
+    const query = Object.fromEntries(params.entries());
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify({
+      parts,
+      query,
+    }));
+  });
 }
 
 module.exports = {
