4a86d0f701332b1b73c5719dcfd1e4f8d318774d
Dmytro <dmytro.savchenko@similarweb.com>
2024-03-25 08:33:50 +0200

    impl node_params-server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    const formattedUrl = req.url.replace(/\/+/g, '/');
+    const normalizedUrl = new URL(formattedUrl, `http://${req.headers.host}`);
+
+    const parts = normalizedUrl.pathname.slice(1).split('/');
+    const query = Object.fromEntries(normalizedUrl.searchParams);
+
+    const data = {
+      parts,
+      query,
+    };
+
+    res.end(JSON.stringify(data));
+  });
+
+  return server;
 }
 
 module.exports = {
