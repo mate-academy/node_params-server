7b0b686d3798431299ed5684374798a535cf1925
Dmytro <dmytro.savchenko@similarweb.com>
2024-03-26 20:03:17 +0200

    handle search params with same key

--- src/createServer.js
+++ src/createServer.js
@@ -11,7 +11,7 @@ function createServer() {
     const normalizedUrl = new URL(formattedUrl, `http://${req.headers.host}`);
 
     const parts = normalizedUrl.pathname.slice(1).split('/');
-    const query = Object.fromEntries(normalizedUrl.searchParams);
+    const query = getSearchParams(normalizedUrl.search);
 
     const data = {
       parts,
@@ -23,6 +23,30 @@ function createServer() {
 
   return server;
 }
+
+const getSearchParams = (search) => {
+  return search
+    .slice(1)
+    .split('&')
+    .reduce((acc, item) => {
+      const [key, value] = item.split('=');
+
+      if (acc.hasOwnProperty(key)) {
+        if (Array.isArray(acc[key])) {
+          acc[key].push(value);
+
+          return acc;
+        }
+
+        acc[key] = [acc[key], value];
+
+        return acc;
+      }
+      acc[key] = value;
+
+      return acc;
+    }, {});
+};
 
 module.exports = {
   createServer,
