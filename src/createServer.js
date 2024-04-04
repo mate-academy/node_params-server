e03ad1664e3a06ff07a340c030d7aa134a2f56a3
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-04-04 18:05:28 +0200

    add node_params-server solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    let newUrl = req.url;
+
+    console.log(req.url);
+
+    if (req.url.slice(0, 2) === '//') {
+      newUrl = req.url.slice(1);
+    }
+
+    const { pathname, searchParams } = new URL(
+      newUrl,
+      `http://${req.headers.server}`,
+    );
+
+    const parts = pathname.replaceAll('/', ' ').trim().split(' ');
+
+    const query = Object.fromEntries(searchParams.entries());
+
+    res.end(JSON.stringify({ parts, query }));
+  });
+
+  return server;
 }
 
 module.exports = {
