82a5b49bccae66fd4b98fcd060914c6f51e09bc8
foxstudent103376 <yashopua@gmail.com>
2024-04-25 17:08:56 +0300

    add SOLUTION

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,41 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    const normalizedUrl = new URL(req.url, 'http://localhost:5701');
+
+    const query = Object.fromEntries(normalizedUrl.searchParams.entries());
+    const parts = [];
+    const urlParams = req.url.split('?')[0];
+    const urlParamsArr = urlParams.split('/');
+
+    urlParamsArr.forEach((part) => {
+      if (part) {
+        parts.push(part);
+      }
+    });
+
+    try {
+      res.statusCode = 200;
+
+      const respBody = {
+        parts,
+        query,
+      };
+
+      res.end(JSON.stringify(respBody));
+    } catch (error) {
+      res.statusCode = 400;
+      res.end(JSON.stringify({ errors: [{ message: error.message }] }));
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
