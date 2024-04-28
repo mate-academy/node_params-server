be92bf3bb11144608be5f2e9ed0f81594b122c38
OleksiiMishchenko <alex.mishch@gmail.com>
2024-04-28 05:35:11 +0300

    Params server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const { pathname, searchParams } = new URL(
+      req.url.slice(1),
+      `http://${req.headers.host}`,
+    );
+    const parts = pathname.slice(1).split('/');
+    const query = Object.fromEntries(searchParams.entries());
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ parts, query }));
+  });
 }
 
 module.exports = {
