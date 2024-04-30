f8f59bc77801f777772b5da179ae3dcb32957b88
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-04-30 11:49:08 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,31 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const paramsServer = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const [path, params] = req.url.split('?');
+    const parts = path.split('/').filter((part) => part !== '');
+    const queryParams = new URLSearchParams(params);
+
+    const query = {};
+
+    for (const [key, value] of queryParams.entries()) {
+      query[key] = value;
+    }
+
+    res.end(
+      JSON.stringify({
+        parts: parts,
+        query: query || {},
+      }),
+    );
+  });
+
+  return paramsServer;
 }
 
 module.exports = {
