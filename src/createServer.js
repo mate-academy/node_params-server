4c7dcbc344193feae1284d21faa7f62b19e40eff
Denis Kotov <dionisiy2904@gmail.com>
2024-05-08 17:21:12 +0300

    node.js params server

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,36 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    const query = {};
+    parsedUrl.searchParams.forEach((value, key) => {
+      query[key] = value;
+    });
+
+    const splitUrl = req.url.split('?');
+
+    const parts = splitUrl[0]
+      .split('/')
+      .filter((part) => part !== '');
+
+    const result = {
+      parts: parts,
+      query: query,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
 }
 
 module.exports = {
