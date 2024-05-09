e7553a890a4a9d282c57ab906137354f213b1fbf
Rafael <rafmamedov@gmail.com>
2024-05-09 13:37:39 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,25 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const { sendResponse, parseUrl } = require('./utils/helpers');
+const { statusCodes, errorMessages } = require('./utils/constants');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const { OK, INTERNAL_SERVER_ERROR } = statusCodes;
+    const parsedData = parseUrl(req);
+
+    try {
+      sendResponse(res, OK, parsedData);
+    } catch {
+      sendResponse(res, INTERNAL_SERVER_ERROR, {
+        errors: [{ message: errorMessages.internalError }],
+      });
+    }
+  });
+
+  return server;
 }
 
 module.exports = {
