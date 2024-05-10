86375a5f405c24e65141f6e2561801352c44af8a
Ihor Bahniuk <hattorikanto@gmail.com>
2024-05-10 03:11:56 +0300

    task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,28 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const url = require('node:url');
+
+const { getPathnameParts } = require('./getPathnameParts');
+const { getQueryParts } = require('./getQueryParts');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const path = url.parse(req.url).pathname || '';
+    const params = url.parse(req.url).query || '';
+
+    res.writeHead(200, { 'Content-type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        parts: getPathnameParts(path),
+        query: getQueryParts(params),
+      }),
+    );
+  });
+
+  return server;
 }
 
 module.exports = {
