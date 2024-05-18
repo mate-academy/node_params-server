12230993b0ee59ad2293eed8d3d2b0aa5ecd70d5
Andrii Khomiak <an.khomiak@gmail.com>
2024-05-18 21:49:36 +0300

    task done

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,29 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const url = require('url');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = new http.Server();
+
+  server.on('request', (req, res) => {
+    const nomalizedUrl = new url.URL(
+      req.url.replace(/\/+/g, '/'),
+      `http://${req.headers.host}`,
+    );
+
+    const parts = nomalizedUrl.pathname
+      .split('/')
+      .filter((item) => item !== '');
+
+    const query = Object.fromEntries(nomalizedUrl.searchParams.entries());
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ parts, query }));
+  });
+
+  return server;
 }
 
 module.exports = {
