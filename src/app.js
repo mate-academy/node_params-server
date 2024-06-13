0cc996fb3eade61ad01a08796bdf967275cdf299
Justin <thedarkvibe1@gmail.com.com>
2024-06-13 12:58:58 +0200

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,5 @@
 const { createServer } = require('./createServer');
 
 createServer().listen(5701, () => {
-  console.log(`Server is running on http://localhost:${5701} 🚀`);
-  console.log('Available at http://localhost:5700');
+  console.log('Available at http://localhost:5701');
 });
--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const url = require('url');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    res.setHeader('content-type', 'application/json');
+    res.statusCode = 200;
+
+    const reqUrl = new url.URL('http://localhost:5701' + req.url);
+    const paths = reqUrl.pathname
+      .split('/')
+      .slice(1)
+      .filter((path) => path !== '');
+
+    const params = {};
+
+    reqUrl.search
+      .slice(1)
+      .split('&')
+      .forEach((param) => {
+        const [k, v] = param.split('=');
+
+        params[k] = v === '' ? '' : v;
+      });
+
+    res.end(JSON.stringify({ parts: paths, query: params }));
+  });
 }
 
 module.exports = {
