8c914e674d3c66830583fddfac8d9b7967a0737f
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-18 13:49:36 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,33 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const normalizedURL = new URL(
+      path.normalize(req.url),
+      `http://${req.headers.host}`,
+    );
+    const searchParams = Object.fromEntries(
+      normalizedURL.searchParams.entries(),
+    );
+    const pathName = normalizedURL.pathname.slice(1);
+    const getPathArr = pathName
+      .split('/')
+      .filter((el) => el.trim().length !== 0);
+    const responseObj = {
+      parts: getPathArr,
+      query: searchParams,
+    };
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(responseObj));
+  });
+
+  return server;
 }
 
 module.exports = {
