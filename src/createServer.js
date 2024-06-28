5223081cc2c4a96fa03d653cf8d3b260b6226c86
eduvinagre <eduvinagre@gmail.com>
2024-06-28 10:59:44 -0300

    adding task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,29 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const reqUrl = new URL(`http://${req.headers.host}${req.url}`);
+
+    const parts = reqUrl.pathname.split('/').filter((part) => part !== '');
+
+    const query = {};
+
+    reqUrl.searchParams.forEach((value, key) => {
+      query[key] = value;
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+      JSON.stringify({
+        parts: parts,
+        query,
+      }),
+    );
+  });
 }
 
 module.exports = {
