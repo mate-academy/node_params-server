1a4173547d49a18d7ba6658a8ac18350e0f2ff18
eduvinagre <eduvinagre@gmail.com>
2024-06-28 11:25:25 -0300

    adding code improvements

--- src/createServer.js
+++ src/createServer.js
@@ -6,7 +6,6 @@ const http = require('http');
 function createServer() {
   return http.createServer((req, res) => {
     const reqUrl = new URL(`http://${req.headers.host}${req.url}`);
-
     const parts = reqUrl.pathname.split('/').filter((part) => part !== '');
 
     const query = {};
@@ -15,14 +14,13 @@ function createServer() {
       query[key] = value;
     });
 
-    res.setHeader('Content-Type', 'application/json');
+    const response = {
+      parts: parts,
+      query: query,
+    };
 
-    res.end(
-      JSON.stringify({
-        parts: parts,
-        query,
-      }),
-    );
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response));
   });
 }
 
