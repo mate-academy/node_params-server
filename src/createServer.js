ed2ee7d1538a50d689bc2fafa587a35fc6b2243b
Allanpt <jatallantavares@gmail.com>
2024-07-01 10:08:59 -0300

    refactoring server

--- src/createServer.js
+++ src/createServer.js
@@ -8,18 +8,9 @@ function createServer() {
     const params = new URL(`http://${req.headers.host}${req.url}`);
     const parts = params.pathname.split('/').filter((el) => el !== '');
 
-    const queries = [];
-
-    for (const qury of params.searchParams) {
-      const nameQuery = qury[0];
-      const targetCase = qury[1];
-
-      queries.push([nameQuery, targetCase]);
-    }
-
     const query = {};
 
-    for (const qury of queries) {
+    for (const qury of params.searchParams) {
       query[qury[0]] = qury[1];
     }
 
