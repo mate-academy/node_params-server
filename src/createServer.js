9f53f6899a536da719be3dc76e5f3f91824b331e
volodymyr <volodiayurovych@gmail.com>
2024-07-07 21:56:46 +0200

    check

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,35 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const result = {};
+
+    const reqUrl = req.url;
+    const reqUrlSplited = reqUrl.split('?');
+
+    result.parts = reqUrlSplited[0].split('/').splice(1);
+
+    const params = new URLSearchParams(reqUrlSplited[1]);
+
+    const allParams = {};
+
+    params.forEach((value, key) => {
+      allParams[key] = value;
+    });
+
+    result.query = allParams;
+
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
 }
 
 module.exports = {
