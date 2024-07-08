76da9febe3371c5f4e0c27209ec0646b7e519a95
Dmytro Somik <dmytro.somik@mccann.com>
2024-07-08 14:47:19 +0100

    solved

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,37 @@
 /* eslint-disable no-console */
 'use strict';
 
+// Write code here
+// Also, you can create additional files in the src folder
+// and import (require) them here
+
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const protocol = req.socket.encrypted ? 'https' : 'http';
+    const hostName = `${protocol}://${req.headers.host}/${req.url}`;
+    const parsedUrl = new URL(hostName);
+
+    const parts = parsedUrl.pathname.split('/').filter((x) => x);
+    const query = Object.fromEntries(parsedUrl.searchParams.entries());
+
+    const response = {
+      parts,
+      query,
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify(response));
+  });
 }
+
+module.exports = {
+  createServer,
+};
 
 module.exports = {
   createServer,
