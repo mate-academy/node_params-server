ecf6c20cf070ca6b981a1651c8a5d5caf6b6142b
Arthur Tolkachov <babanjerelo@gmail.com>
2024-08-04 20:19:21 +0300

    [Feature] added query and parts extractor

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
-/* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const normalizedURL = new URL(
+      req.url.replace('//', '/'),
+      'http://localhost:5701',
+    );
+
+    const parts = normalizedURL.pathname.slice(1).split('/');
+    const query = {};
+
+    normalizedURL.searchParams.forEach((value, key) => {
+      query[key] = value ?? '';
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({ parts, query }));
+    res.end();
+  });
+
+  return server;
 }
 
 module.exports = {
