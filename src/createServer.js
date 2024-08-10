3f017bc3f9154ed687a017827ce507037820db37
deezhizyu <kalenskuja@gmail.com>
2024-08-10 18:37:07 +0200

    Implement params echo

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,28 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const url = new URL(
+      req.url.replace(/\/\//g, ''),
+      `http://${req.headers.host}`,
+    );
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = '200';
+    res.statusMessage = 'OK';
+
+    res.end(
+      JSON.stringify({
+        parts: url.pathname.slice(1).split('/'),
+        query: Object.fromEntries(url.searchParams.entries()),
+      }),
+    );
+  });
+
+  return server;
 }
 
 module.exports = {
