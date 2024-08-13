df4c1bd78b0a4981368f84687b38522e1395c849
Victor Hrygorash <viktor12041991@i.ua>
2024-08-13 10:50:29 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,30 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    try {
+      const url = new URL(req.url, `http://${req.headers.host}`);
+
+      const pathname = url.pathname || '';
+      const parts = pathname.split('/').filter((part) => part.length > 0);
+
+      const query = Object.fromEntries(url.searchParams.entries());
+
+      const responseObject = {
+        parts: parts,
+        query: query,
+      };
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(responseObject));
+    } catch (error) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Invalid URL' }));
+    }
+  });
 }
 
 module.exports = {
