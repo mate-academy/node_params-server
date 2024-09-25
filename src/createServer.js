8da00e6461348ea45b235a39f8d4b6612ea15ff3
Yurii Yepikhov <yuriiyepikhov@gmail.com>
2024-09-26 01:12:30 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,31 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const normalizedURL = new URL(
+      req.url.replaceAll('//', '/'),
+      `http://${req.headers.host}`,
+    );
+
+    const parts = normalizedURL.pathname
+      .split('/')
+      .filter((part) => part !== '');
+
+    const query = Object.fromEntries(normalizedURL.searchParams.entries());
+
+    const data = {
+      parts,
+      query,
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+  });
+
+  return server;
 }
 
 module.exports = {
