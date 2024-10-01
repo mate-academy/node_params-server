b8a202979d25a959d1b619fc4d950be9ce4369d3
mrsvolodya <vovamursky@gmail.com>
2024-10-01 17:08:40 +0300

    improve

--- src/createServer.js
+++ src/createServer.js
@@ -5,26 +5,14 @@ const http = require('node:http');
 
 function createServer() {
   const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-
-    const url = new URL(req.url, `http://${req.headers.host}`);
+    const url = new URL(req.url.slice(1), `http://${req.headers.host}`);
     const parts = url.pathname.split('/').filter((v) => v);
-
-    if (url.hostname !== 'localhost') {
-      parts.push(url.hostname);
-    }
-
     const search = url.searchParams.entries();
     const query = Object.fromEntries(Array.from(search));
 
-    const result = {
-      parts: parts,
-      query,
-    };
-
+    res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
-
-    res.end(JSON.stringify(result));
+    res.end(JSON.stringify({ parts, query }));
   });
 
   return server;
