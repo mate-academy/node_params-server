bd345bc30a93fe789f5e8d17e5c815cd2a349576
Andrii <andriykostiuk1@gmail.com>
2024-11-11 21:43:45 +0200

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,30 @@
-/* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    const url = new URL(req.url.slice(1), `http://${req.headers.host}`);
+
+    if (url.pathname === '/favicon.ico') {
+      res.end();
+
+      return;
+    }
+
+    const result = {
+      parts: url.pathname.split(/\/+/).filter((v) => !!v),
+      query: {},
+    };
+
+    url.searchParams.forEach((value, key) => {
+      result.query[key] = value;
+    });
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(result));
+  });
 }
 
 module.exports = {
