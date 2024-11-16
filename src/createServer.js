49b387da9753de4222935cd638530a03ae479440
Iryna Lysko <goiralysko@gmail.com>
2024-11-16 22:48:33 +0200

    add task

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const responseObject = {};
+
+    const [path, params] = req.url.split('?');
+    const parts = path.split('/').filter(Boolean);
+    const query = Object.fromEntries(new URLSearchParams(params).entries());
+
+    if (parts.length) {
+      responseObject.parts = [...parts];
+    }
+
+    responseObject.query = { ...query };
+
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(responseObject, null, 2));
+  });
+
+  return server;
 }
 
 module.exports = {
