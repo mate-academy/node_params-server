7166b1408dff7252f326f2cfe4a918e910924359
jdifek <givotkov11@gmail.com>
2024-11-23 12:15:09 +0200

    sfds

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,27 @@
-/* eslint-disable no-console */
-'use strict';
+const http = require('http');
 
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer((req, res) => {
+    // Use URL to parse the request URL
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    // Normalize pathname and split it into parts
+    const parts = parsedUrl.pathname.split('/').filter(Boolean);
+
+    // Handle query parameters, ensuring empty ones are not omitted
+    const query = {};
+
+    parsedUrl.searchParams.forEach((value, key) => {
+      // If the parameter is empty, we still want it to be included in the query
+      query[key] = value || ''; // Ensure even empty values are present
+    });
+
+    // Construct the response JSON
+    const responseData = { parts, query };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(responseData));
+  });
 }
 
 module.exports = {
