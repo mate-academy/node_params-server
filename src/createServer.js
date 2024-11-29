5ac275dab83ec6da29d2698258c4d2aa4e0c2ed8
Olya <olyavidzi97@gmail.com>
2024-11-29 20:34:22 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,31 @@
 /* eslint-disable no-console */
 'use strict';
 
-function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
-}
+const http = require('http');
+
+const PORT = process.env.PORT || 5701;
+const baseUrl = `http://localhost:${PORT}`;
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    const normalizedURL = new URL(req.url, baseUrl);
+    const parts = normalizedURL.pathname.split('/').slice(1);
+    const params = {};
+
+    for (const [key, value] of normalizedURL.searchParams.entries()) {
+      params[key] = value;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(
+      JSON.stringify({
+        parts: parts,
+        query: params,
+      }),
+    );
+  });
+};
 
 module.exports = {
   createServer,
